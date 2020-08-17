851b905b8b68267c30125b8a86c9b36907d0c978
Heewon <ccc4804@naver.com>
2020-08-18 01:00:03 +0900

    Chapter7 - CSS Modules

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,41 +1,16 @@
-import React from 'react';
-import Button from './components/Button';
-import './components/App.scss'
+import React, { useState } from "react";
+import CheckBox from "./components/CheckBox";
 
 function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = (e) => {
+    setCheck(e.target.checked);
+  };
   return (
-    <div className="App">
-      <div className="buttons">
-        <Button color="blue" size="large">BUTTON</Button>
-        <Button color="blue" >BUTTON</Button>
-        <Button color="blue" size="small">BUTTON</Button>
-      </div>
-      <div className="buttons">
-        <Button color="gray" size="large">BUTTON</Button>
-        <Button color="gray" >BUTTON</Button>
-        <Button color="gray" size="small">BUTTON</Button>
-      </div>
-      <div className="buttons">
-        <Button color="pink" size="large">BUTTON</Button>
-        <Button color="pink" >BUTTON</Button>
-        <Button color="pink" size="small">BUTTON</Button>
-      </div>
-      <div className="buttons">
-        <Button color="blue" size="large" outline>BUTTON</Button>
-        <Button color="gray" outline={true}>BUTTON</Button>
-        <Button color="pink" size="small" outline={true}>BUTTON</Button>
-      </div>
-      <div className="buttons">
-        <Button color="blue" size="large" fullWidth>BUTTON</Button>
-        <Button color="gray" size="large" fullWidth>BUTTON</Button>
-        <Button color="pink" size="large" fullWidth
-        onClick={()=>{
-          console.log("Click button");
-        }}
-        onMouseMove={() => {
-          console.log("Mouse Move")
-        }}>BUTTON</Button>
-      </div>
+    <div>
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
     </div>
   );
 }
