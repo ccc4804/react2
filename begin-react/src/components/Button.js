8f38af0c28247231e58ff0b9338b00bb9dca43b6
Heewon <ccc4804@naver.com>
2020-08-17 18:05:03 +0900

    Chapter3 - Saas 버튼 A - size Props 설정하기

--- begin-react/src/components/Button.js
+++ begin-react/src/components/Button.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import classNames from 'classnames'
+
 import './Button.scss';
 
-function Button({children}) {
+
+//size : large, medium, small
+function Button({children, size}) {
   return (
-    <button className="Button">{children}</button>
+    //<button className={["Button", size].join(' ')}>{children}</button>
+    //<button className={`Button ${size}`}>{children}</button> // 위와 동일하다.
+    <button className={classNames('Button', size)}>{children}</button> // 위와 동일하다.
   );
 }
+
+Button.defaultProps = {
+    size: 'medium'
+};
 
 export default Button;
