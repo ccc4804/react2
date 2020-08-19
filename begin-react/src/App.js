f559ed822675e0900c886111efd91e3779a885db
Heewon <ccc4804@naver.com>
2020-08-19 16:26:29 +0900

    Chapter13 - Styled-components - Dialog 만들

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import Button from "./components/Button";
+import Dialog from "./components/Dialog";
 
 const AppBlock = styled.div`
   width: 512px;
@@ -23,31 +24,41 @@ const ButtonGroup = styled.div`
 `;
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  };
+  const onConfirm = () => {
+    console.log('확인')
+    setDialog(false);
+  };
+  const onCancel = () => {
+    console.log('취소')
+    setDialog(false);
+  };
+
   return (
+    // ThemProvider는 하나의 엘리먼트를 가져야한다.
     <ThemeProvider theme={{ palette }}>
-      <AppBlock>
-        <ButtonGroup>
-          <Button size="small">BUTTON</Button>
-          <Button color="gray">BUTTON</Button>
-          <Button color="pink" size="large">
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size="small" outline>BUTTON</Button>
-          <Button color="gray" outline>BUTTON</Button>
-          <Button color="pink" size="large" outline>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size="large" fullWidth>BUTTON</Button>
-          <Button color="gray" size="large" fullWidth>BUTTON</Button>
-          <Button color="pink" size="large" fullWidth>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-      </AppBlock>
+      <>
+        <AppBlock>
+          <ButtonGroup>
+            <Button color="pink" size="large" onClick={onClick}>
+              삭제
+            </Button>
+          </ButtonGroup>
+        </AppBlock>
+        <Dialog
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancelText="취소"
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+          visible={dialog}
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
     </ThemeProvider>
   );
 }
